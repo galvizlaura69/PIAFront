8c84e64312cae53ec6874de9d4bb45693dff097c
KatheGalviz <laura.galviz@finanzauto.com.co>
2024-04-06 13:41:00 -0500

    cambios

---
+++ src/src/users/hooks/createUser.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { API } from '../../config/dummyapi';
+
+
+  const createUser = async (payload) => {
+    try{
+    console.log(payload, 'payload');
+    const { data } = await axios.post(`${API}/users`, payload);
+    const users = data.data;
+    console.log('response', data.data)
+    return users;
+    }catch(error){
+      return error.message;
+    }
+  };
+
+  export default createUser;
