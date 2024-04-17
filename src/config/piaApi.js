c71f4ffc675675e39f5229fc02a01a6dfe524602
KatheGalviz <laura.galviz@finanzauto.com.co>
2024-04-17 16:34:01 -0500

    ajustes local

--- src/config/piaApi.js
+++ src/config/piaApi.js
@@ -1,4 +1,6 @@
-const API = process.env.REACT_APP_BACKEND_URL || "http://localhost:3010";
+const API = process.env.REACT_APP_BACKEND_URL || "https://intense-sea-07667-7fa013b83aac.herokuapp.com";
+//descomentar linea para correr en local
+//const API =  "http://localhost:3010";
 const HEADERS = {};
 
 export {
