ba1269f9d4b0ca284f1de25202e9d55d562aad60
KatheGalviz <laura.galviz@finanzauto.com.co>
2024-04-25 14:25:07 -0500

    ajustes img

--- src/users/components/SensorData.js
+++ src/users/components/SensorData.js
@@ -43,6 +43,13 @@ class SensorData extends Component {
     this.setState({ filterColor: event.target.value });
   };
 
+  formatDate = (dateString) => {
+    const createdAt = new Date(dateString);
+    const formattedDate = `${createdAt.getDate()}/${createdAt.getMonth() + 1}/${createdAt.getFullYear()}`;
+    const formattedTime = `${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}`;
+    return `Fecha: ${formattedDate} / Hora: ${formattedTime}`;
+  };
+
   render() {
     const { dataSensorList, filterColor } = this.state;
 
@@ -51,40 +58,61 @@ class SensorData extends Component {
       ? dataSensorList
       : dataSensorList.filter(data => this.getCO2Color(parseInt(data.co2Level)) === filterColor);
 
+
+
     return (
-      <div style={{ width: '50%', margin: 'auto', padding: '16px' }}>
-        <Typography variant="h5" gutterBottom style={{ color: '#154360', textAlign: 'center', marginBottom: '15px', height: 'auto', fontSize: '40px', fontWeight: 'bold' }}>
+      <div style={{ width: '80%', margin: 'auto', padding: '16px' }}>
+        <Typography variant="h5" gutterBottom style={{ color: '#154360', textAlign: 'center', marginBottom: '25px', height: 'auto', fontSize: '40px', fontWeight: 'bold' }}>
           DATOS SENSOR
         </Typography>
-        <Typography variant="filtro" style={{  marginBottom: '-30px', color:  '#154360', fontWeight:'bold'  }}>
-          Filtro por niveles
-        </Typography>
-        <FormControl fullWidth style={{ marginBottom: '15px' }}>
-          <Select
-            labelId="color-filter-label"
-            id="color-filter"
-            value={filterColor}
-            onChange={this.handleFilterChange}
-          >
-            <MenuItem value="all">Todos</MenuItem>
-            <MenuItem value="green">Bajo</MenuItem>
-            <MenuItem value="orange">Medio</MenuItem>
-            <MenuItem value="red">Alto</MenuItem>
-          </Select>
-        </FormControl>
-        {filteredData.length > 0 ? (
-          filteredData.map((data, index) => (
-            <div key={index} style={{ marginBottom: '2px', padding: '4px', borderRadius: '4px', textAlign: 'center' }}>
-              <Typography variant="subtitle1" style={{ color: this.getCO2Color(parseInt(data.co2Level)), border: "1px solid", fontSize: '16px', fontWeight: 'bold' }}>
-                CO2: {parseInt(data.co2Level).toFixed(0)} PPM
+        <div style={{ display: 'flex', width: '100%', margin: 'auto' }}>
+          <div style={{ width: '75%', marginRight: '30px' }}>
+            <Typography variant="filtro" style={{ marginBottom: '-30px', color: '#154360', fontWeight: 'bold' }}>
+              Filtrar por niveles
+            </Typography>
+            <FormControl fullWidth style={{ marginBottom: '15px' }}>
+              <Select
+                labelId="color-filter-label"
+                id="color-filter"
+                value={filterColor}
+                onChange={this.handleFilterChange}
+              >
+                <MenuItem value="all">Todos</MenuItem>
+                <MenuItem value="green">Bajo</MenuItem>
+                <MenuItem value="orange">Medio</MenuItem>
+                <MenuItem value="red">Alto</MenuItem>
+              </Select>
+            </FormControl>
+            {filteredData.length > 0 ? (
+              filteredData.map((data, index) => (
+                <div key={index} style={{ marginBottom: '2px', padding: '4px', borderRadius: '4px', textAlign: 'center' }}>
+                  <Typography variant="subtitle1" style={{ color: this.getCO2Color(parseInt(data.co2Level)), border: "1px solid", fontSize: '16px', fontWeight: 'bold' }}>
+                    CO2: {parseInt(data.co2Level).toFixed(0)} PPM / <span>{this.formatDate(data.createdAt)}</span>
+                  </Typography>
+                </div>
+              ))
+            ) : (
+              <Typography variant="subtitle1" style={{ textAlign: 'center', color: 'gray', marginTop: '20px' }}>
+                No hay registros de datos para este nivel.
               </Typography>
-            </div>
-          ))
-        ) : (
-          <Typography variant="subtitle1" style={{ textAlign: 'center', color: 'gray', marginTop: '20px' }}>
-            No hay registros de datos para este nivel.
-          </Typography>
-        )}
+            )}
+          </div>
+
+          <div style={{ textAlign: 'center', width: '25%', margin: 'auto', marginTop: '20px', border: '1px solid #ccc', padding: '10px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
+            <Typography variant='subtitle2' style={{ color: '#154360', textAlign: 'center', height: 'auto', fontSize: '15px', fontWeight: 'bold' }}>
+              ESCALA DE NIVELES
+            </Typography>
+            <img src="/escala.jpg" alt="Escala" style={{ width: '100%', height: 'auto' }} />
+            <Typography variant="subtitle1" style={{ fontSize: '12px', fontWeight: 'bold' }}>
+              <span style={{ color: 'green' }}> Nivel Bajo: Menor o igual a 400 PPM</span>
+              <br></br>
+              <span style={{ color: 'orange' }}> Nivel Medio: Entre 401 PPM y 700 PPM</span>
+              <br></br>
+              <span style={{ color: 'red' }}> Nivel Alto:  Mayor a 700 PPM</span>
+            </Typography>
+          </div>
+
+        </div>
       </div>
     );
   }
